555ee99fd61f9c2b2a16abc143cc7ea1e1cf93d6
Emmanuel Eze <Emmanuel.Eze@mainone.net>
2022-10-29 06:59:52 +0100

    hngmodified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-//import './App.css';
-import "./index.css";
+import './index.css'
 
 function App() {
   return (
     <div className="App">
-      <div className='profile_pics'>
-      <img src='NEW PIC.jpg' className='image'/>
-     <div id='slackName'>@Emmanuel</div>
+      <div className='profile'>
+      <img src='NEW PIC.jpg' id="profile__img" className='image'/>
+     <div id='twitter'>ezeemmachukwu</div>
+     <div id='slack'>Emmanuel</div>
       </div>
-        
+
       <div className='container'>
       
-      <a href='https://twitter.com/ezeemmachukwu'><button className='twitter' id="links">Twitter Link</button></a>
-       <a href='https://training.zuri.team/'><button className='zuri' id="links">Zuri Team</button> </a>
-       <a href='https://books.zuri.team/'><button className='books' id ="links">Zuri Books</button></a>
-       <a href='https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E'><button className='python' id="links">Python Books</button></a>
-       <a href='https://background.zuri.team/'><button className='colors' id='links'>Background check for Colors</button></a>
-       <a href='https://books.zuri.team/design-rules'><button className='design' id ="links">Design Books</button></a>
+      <a href='https://twitter.com/ezeemmachukwu'><button className="links">Twitter Link</button></a>
+       <a href='https://training.zuri.team/'><button id='btn__zuri' className="links">Zuri Team</button> </a>
+       <a href='https://books.zuri.team/'><button id='books' className ="links">Zuri Books</button></a>
+       <a href='https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E'><button id='book__python' className="links">Python Books</button></a>
+       <a href='https://background.zuri.team/'><button id='pitch' className='links'>Background check for Colors</button></a>
+       <a href='https://books.zuri.team/design-rules'><button id='book__design' className ="links">Design Books</button></a>
        <img src='logo3.PNG' className='hnglogo3'></img>
      </div>
       <footer>
@@ -25,7 +24,6 @@ function App() {
         <div><small>HNG internship 9 Frontend Task</small></div>
         <img src='ingressive.png' className='logo2'></img>
       </footer>
-      
     </div>
   );
 }
